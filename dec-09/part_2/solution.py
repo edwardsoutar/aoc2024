f0fd9353ac57e3d6ae51d38d099f2dcf6b6cdaec
edward.soutar <esoutar@live.com>
2024-12-10 18:13:09 -0500

    Improve performance of day 9 part 2 solutions

--- dec-09/part_2/solution.py
+++ dec-09/part_2/solution.py
@@ -7,7 +7,7 @@ def parse_input():
     file = open(os.path.join(dir,'input.txt'), "r")
 
     disk = []
-    spaces = {}
+    spaces = []
     files = {}
 
     for line in file:
@@ -25,7 +25,7 @@ def parse_input():
                 index += file_size
             else:
                 space_size = int(char)
-                spaces[index] = space_size
+                spaces.append([index, space_size])
                 disk += ["."] * space_size
                 is_even = True
                 index += space_size
@@ -33,6 +33,15 @@ def parse_input():
     file.close()
 
     return disk, spaces, files
+
+def move_file_block(disk, file_index, space_index, file_size):
+    id = disk[file_index]
+
+    for i in range(file_size):
+        disk[file_index - i] = '.'
+        disk[space_index + i] = id
+
+    return disk
 
 def move_file_blocks(disk, spaces, files):
     right = len(disk) - 1
@@ -43,26 +52,22 @@ def move_file_blocks(disk, spaces, files):
             right -= 1
         else:
             file_size = files[disk[right]]
-            id = disk[right]
+            
+            for i in range(len(spaces)):
+                if spaces[i][0] >= right:
+                    continue
 
-            # Left moves forward until finds a space large enough to contain right
-            left, left_end = 0, 0
-
-            while left_end < right and left_end - left < file_size:
-                if disk[left] != '.':
-                    left += 1
-                    left_end = left
-                elif disk[left_end] != '.':
-                    left = left_end
-                else:
-                    left_end += 1
-                    
-            if left_end - left == file_size:
-                for i in range(file_size):
-                    disk[left + i] = id
-                    disk[right - i] = '.'
+                if spaces[i][1] > file_size:
+                    disk = move_file_block(disk, right, spaces[i][0], file_size)
+                    spaces[i][0] += file_size
+                    spaces[i][1] -= file_size
+                    break
+                elif spaces[i][1] == file_size:
+                    disk = move_file_block(disk, right, spaces[i][0], file_size)
+                    spaces.pop(i)
+                    break
             else:
-                right -= files[id]
+                right -= file_size
             
     return disk
 
